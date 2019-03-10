14b4d10c43cebf9beed32a8524a0e27c9b278954
zecheng <ganzecheng1988@gmail.com>
2019-03-10 07:21:25 -0400

    corrected units

--- two_sph_ele_force_by_normal_derivatives.m
+++ two_sph_ele_force_by_normal_derivatives.m
@@ -2,7 +2,7 @@
 %the surface normal derivative on grids, governed by Poisson equantion, and solved by BEM.
 
 %%%%%%%%%%about input files%%%%%%%%%%%%%%%%%%%%%%%%
-%All we need is the location of each collocation point, the normal derivative on each collocation point, and
+%All we need is the location of each collocation point, the normal derivative on each collocation point,
 %and the weights for each point (I am using CC, so the weights are the triangle areas, but for other schemes, the force can be calculated as well).
 %these information are saved in data.dat
 
@@ -15,21 +15,24 @@
 
 data=load('data.dat');
 %load the collocation pts locations, normal derivative value, and weights from the data file
-grid_loc=data(:,2:4);
-normal_dev=data(:,8);
-weights=data(:,10);
+grid_loc=data(:,2:4); 
+normal_dev=data(:,8); %the surface phi norm derv
+weights=data(:,10); 
+
+normal_dev=normal_dev/4/pi; %rescale the unit of normal_dev
 
 %load the source/dielectric object information
 sourcedata=load('source.dat');
-src_loc=sourcedata(:,2:4); %location of the central charges
-src_char=sourcedata(:,5); %magnitude of the central charges
-eps_i=sourcedata(1,6); %now we assume each sphere has the same dielectric constant inside
-eps_s=sourcedata(1,7);
-N=sourcedata(:,8); %the number of triangles on each sphere
+src_loc=sourcedata(:,2:4); %location of the central charges, same as chrpos(1:3,i)
+src_char=sourcedata(:,5); %magnitude of the central charges, same as atmchr(i)
+eps_i=sourcedata(1,6); %now we assume each sphere has the same dielectric constant inside (same as eps0)
+eps_s=sourcedata(1,7); %eps1
+N=sourcedata(:,8); %the number of triangles on each sphere %same as nface/N, here N=2
 
 index=zeros(2,2); %record the start and end index for each sphere
-index(1,1)=1; index(1,2)=N(1);
-index(2,1)=N(1)+1; index(2,2)=N(1)+N(2);
+index(1,1)=1; index(1,2)=N(1); %nface1
+index(2,1)=N(1)+1;%nface1+1 
+index(2,2)=N(1)+N(2);%nface
 
 
 %obtaining the induced surface charge density using Eq.(1.4) in the writeup.
@@ -64,15 +67,15 @@ end
 for i=1:2 %two bodies
     for j=1:2
         if j~=i
-            for k=index(j,1):index(j:2)
+            for k=index(j,1):index(j,2)
             dx=src_loc(i,1)-grid_loc(k,1);
             dy=src_loc(i,2)-grid_loc(k,2);
             dz=src_loc(i,3)-grid_loc(k,3);
             dr=sqrt(dx*dx+dy*dy+dz*dz);
             
-            Fx(i)=Fx(i)-src_char(i)*sigma_b(k)*weights(k)*dx/dr/dr/dr;
-            Fy(i)=Fy(i)-src_char(i)*sigma_b(k)*weights(k)*dy/dr/dr/dr;
-            Fz(i)=Fz(i)-src_char(i)*sigma_b(k)*weights(k)*dz/dr/dr/dr;
+            Fx(i)=Fx(i)+src_char(i)*sigma_b(k)*weights(k)*dx/dr/dr/dr;
+            Fy(i)=Fy(i)+src_char(i)*sigma_b(k)*weights(k)*dy/dr/dr/dr;
+            Fz(i)=Fz(i)+src_char(i)*sigma_b(k)*weights(k)*dz/dr/dr/dr;
             end
         end
     end
@@ -84,15 +87,15 @@ end
 for i=1:2 %two bodies
     for j=1:2
         if j~=i
-            for k=index(i,1):index(i:2)
+            for k=index(i,1):index(i,2)
             dx=grid_loc(k,1)-src_loc(j,1);
             dy=grid_loc(k,2)-src_loc(j,2);
             dz=grid_loc(k,3)-src_loc(j,3);
             dr=sqrt(dx*dx+dy*dy+dz*dz);
             
-            Fx(i)=Fx(i)-src_char(j)*sigma_b(k)*weights(k)*dx/eps_i/dr/dr/dr;
-            Fy(i)=Fy(i)-src_char(i)*sigma_b(k)*weights(k)*dy/eps_i/dr/dr/dr;
-            Fz(i)=Fz(i)-src_char(i)*sigma_b(k)*weights(k)*dz/eps_i/dr/dr/dr;
+            Fx(i)=Fx(i)+src_char(j)*sigma_b(k)*weights(k)*dx/eps_i/dr/dr/dr;
+            Fy(i)=Fy(i)+src_char(j)*sigma_b(k)*weights(k)*dy/eps_i/dr/dr/dr;
+            Fz(i)=Fz(i)+src_char(j)*sigma_b(k)*weights(k)*dz/eps_i/dr/dr/dr;
             end
         end
     end
@@ -103,8 +106,8 @@ end
 for i=1:2 %two bodies
     for j=1:2
         if j~=i
-            for k=index(i,1):index(i:2)
-                for l=index(j,1):index(j:2)
+            for k=index(i,1):index(i,2)
+                for l=index(j,1):index(j,2)
                     
             dx=grid_loc(k,1)-grid_loc(l,1);
             dy=grid_loc(k,2)-grid_loc(l,2);
@@ -119,9 +122,9 @@ for i=1:2 %two bodies
         end
     end
 end
-Fx=Fx/eps_s;
-Fy=Fy/eps_s;
-Fz=Fz/eps_s;
+Fx=Fx*eps_s; 
+Fy=Fy*eps_s; 
+Fz=Fz*eps_s; 
 
 disp('The result is:')
 disp('sphere#1   sphere#2')
